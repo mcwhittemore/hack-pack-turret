343d59df44fd9fc5197e5502f0674bed68130f1c
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2025-10-09 21:31:36 -0400

    Loop roulette (#11)
    * scope vars and fix spelling
    
    * inc mode check when entering password mode
    
    * loopify roulette mode

--- code/modes/roulette.h
+++ code/modes/roulette.h
@@ -1,47 +1,62 @@
-void runRoulette() {
-  Serial.println("ENTERING ROULETTE MODE");
-  // unsigned long startTime = millis();
-  int dartsFired = 0;
-  while (dartsFired <
-         6) { // while we still have darts, stay within this while loop
-    Serial.println("Darts fired: " + String(dartsFired));
-    pitchServoVal = 110;
-    pitchServo.write(pitchServoVal); // set PITCH servo to flat angle
-    yawServo.write(145);             // set YAW to rotate slowly
-    delay(400);                      // rotating for a moment
-    yawServo.write(90);              // stop
-    delay(400 * random(1, 4));       // wait for a random delay each time
+int rouletteModeCheck = 0;
+int rouletteDartsFired = 0;
+bool rouletteRunning = false;
 
-    if (random(3) == 0) { // you have a 1 in six chance of being hit
-      delay(700);
-      if (random(2) == 0) { // 50% chance to either shake the head yes before
-                            // firing or just fire
-        shakeHeadYes();
-        delay(150);
-        fire(); // fire 1 dart
-        delay(100);
-        dartsFired++;
-      } else {
-        fire(); // fire 1 dart
-        delay(50);
-        dartsFired++;
-      }
+void runRoulette() {
+
+  Serial.println("Darts fired: " + String(rouletteDartsFired));
+  pitchServo.write(110);     // set PITCH servo to flat angle
+  yawServo.write(145);       // set YAW to rotate slowly
+  delay(400);                // rotating for a moment
+  yawServo.write(90);        // stop
+  delay(400 * random(1, 4)); // wait for a random delay each time
+
+  if (random(3) == 0) { // you have a 1 in six chance of being hit
+    delay(700);
+    if (random(2) == 0) { // 50% chance to either shake the head yes before
+                          // firing or just fire
+      shakeHeadYes();
+      delay(150);
+      fire(); // fire 1 dart
+      delay(100);
+      rouletteDartsFired++;
     } else {
-      if (random(6) == 5) {
-        delay(700);
-        shakeHeadNo();
-        delay(300);
-      } else {
-        delay(700);
-      }
+      fire(); // fire 1 dart
+      delay(50);
+      rouletteDartsFired++;
+    }
+  } else {
+    if (random(6) == 5) {
+      delay(700);
+      shakeHeadNo();
+      delay(300);
+    } else {
+      delay(700);
     }
   }
-  yawServo.write(90);
+
+  if (rouletteDartsFired >= 6) {
+    yawServo.write(90);
+    rouletteRunning = false;
+  }
 }
 
 bool handleRouletteMode() {
+  if (modeCheck != rouletteModeCheck) {
+    Serial.println("Setting up roulette mode");
+    rouletteDartsFired = 0;
+    rouletteRunning = false;
+    rouletteModeCheck = modeCheck;
+  }
+
   if (inputBuffer[0] == 'K') {
+    rouletteDartsFired = 0;
+    rouletteRunning = true;
+  }
+
+  if (rouletteRunning) {
     runRoulette();
   }
+
   return CLEAR_INPUT_BUFFER;
 }
